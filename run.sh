ba5603eb9a54eff6ece1fd517e48a23e463a786a
Istvan Szukacs <istvan@streambrightdata.com>
2016-04-06 19:03:50 +0200

    Adding initial info

---
+++ run.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+
+#unix install of virtualenv
+virtualenv --version | egrep -q '[0-9]+\.[0-9]+\.[0-9]+' && echo 'virtualenv is installed...' || sudo pip install virtualenv virtualenvwrapper
+
+#
+if [ -e nlp-hackaton/.Python ]; then 
+  echo "nlp-hackaton virtualenv is already created"
+else
+  virtualenv nlp-hackaton
+fi
+source nlp-hackaton/bin/activate
+pip install ipython numpy jupyter matplotlib pandas seaborn scikit-learn sympy plotly --upgrade
+#pip install --upgrade https://storage.googleapis.com/tensorflow/mac/tensorflow-0.6.0-py2-none-any.whl
+jupyter notebook
